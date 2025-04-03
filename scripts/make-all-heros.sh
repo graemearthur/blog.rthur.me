8411bb2e018a9e19678155e435a8ba737d1fe207
Graeme Arthur <Graeme@rthur.me>
2025-04-03 22:32:59 +0100

    update script for hero

--- scripts/make-all-heros.sh
+++ scripts/make-all-heros.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Create hero images for all posts
 
-for INDEX in "${HOME}/Websites/flexiondotorg.github.io/content/posts"/*/index.md; do
+for INDEX in "${HOME}/Documents/blog.rthur.me/content/posts"/*/index.md; do
     echo "Processing ${INDEX}"
     scripts/make-hero.sh "${INDEX}"
 done
 
-for INDEX in "${HOME}/Websites/flexiondotorg.github.io/content/projects"/*/index.md; do
+for INDEX in "${HOME}/Documents/blog.rthur.me/content/projects"/*/index.md; do
     echo "Processing ${INDEX}"
     scripts/make-hero.sh "${INDEX}"
 done
